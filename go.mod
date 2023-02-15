2dd7ae67650ac3c22a1afaa1e3fe46c3d7bd4e40
Umut Tezduyar Lidskog <umuttl@axis.com>
2023-02-15 11:42:38 +0100

    Instrument using the OpenTelemetry API

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/utezduyar/opentelemetry-tracing-quick-start
 
 go 1.19
+
+require go.opentelemetry.io/otel v1.14.0
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
+
+	"go.opentelemetry.io/otel"
 )
 
 // This is an application which happens to include a library (or a module).
@@ -13,13 +16,39 @@ import (
 
 func main() {
 
-	InsertUser("Foo")
+	// Since we don't have any previous context, we take the background one
+	InsertUser(context.Background(), "Foo")
 }
 
 // Below is the library part.
 // ---------------------------------------------------------------------------
 
-func InsertUser(user string) error {
+const name = "module-or-library-name"
+
+func InsertUser(ctx context.Context, user string) error {
+
+	// We are instrumenting the library to create a span. Libraries do this by
+	// linking to the OpenTelemetry API which is in module "go.opentelemetry.io/otel".
+	// Libraries are not allowed to link with the OpenTelemetry SDK layer.
+	//
+	// The Start() API creates a span called InsertUser. The first parameter to it is Context.
+	// This parameters defines if the created span will be child of a parent span.
+	//
+	// We stop this span when the method returns.
+
+	// As long as the application is only linked with the OpenTelemetry API, the spans
+	// created are almost no-operation. Look at ./OpenTelemetryAPI.jpeg image.
+	//
+	// Miscellaneous:
+	// - otel.Tracer() is short for otel.GetTracerProvider().Tracer()
+	// - Span creation should not fail and should not impact the performance according to spec.
+	// - The name to the Tracer should be the name of the module or library
+	// - The span name should be the most general string that identifies an class of Spans.
+	//   InsertUser/foo would be too specific. There are other ways to append specific information
+	//   to a span.
+	//
+	_, span := otel.Tracer(name).Start(ctx, "InsertUser")
+	defer span.End()
 
 	time.Sleep(500 * time.Microsecond)
 	fmt.Println("User has been inserted!")
