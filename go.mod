ea14b9bbb7885bca0395bd87c4cb91e8dddaede5
Umut Tezduyar Lidskog <umuttl@axis.com>
2023-03-17 11:07:37 +0100

    Trace using the SDK

--- go.mod
+++ go.mod
@@ -2,10 +2,15 @@ module github.com/utezduyar/opentelemetry-tracing-quick-start
 
 go 1.19
 
-require go.opentelemetry.io/otel v1.14.0
+require (
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.14.0
+	go.opentelemetry.io/otel/sdk v1.14.0
+)
 
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
 // This is an application which happens to include a library (or a module).
@@ -16,8 +19,45 @@ import (
 
 func main() {
 
+	// Link the application with the OpenTelemetry SDK. We are telling OpenTelemetry
+	// layer what to do with the collected spans. Previous no-operation calls become
+	// meaningful when the application is linked with the OpenTelemetry SDK.
+	// Look at ./OpenTelemetrySDK.jpeg image.
+	//
+	// Libraries/modules are not allowed to link with the OpenTelemetry SDK
+	// according to the specifications.
+	//
+	// Initialize the tracer and prepare what to do during shutdown like flushing.
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatalf("failed to initialize new trace provider: %v", err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Fatalf("error shutting down TracerProvider: %v", err)
+		}
+	}()
+
 	// Since we don't have any previous context, we take the background one
 	InsertUser(context.Background(), "Foo")
+}
+
+func initTracer() (*trace.TracerProvider, error) {
+	// Create an stdout exporter to show the collected spans out to the stdout.
+	stdoutexporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		return nil, fmt.Errorf("creating STDOUT trace exporter: %w", err)
+	}
+
+	// Create a trace provide with given options. Look at ./ObjectDiagram.jpg
+	// to see how things are tied together.
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(stdoutexporter),
+	)
+
+	// Set the global trace provider as what we have created
+	otel.SetTracerProvider(tp)
+	return tp, nil
 }
 
 // Below is the library part.
@@ -47,11 +87,21 @@ func InsertUser(ctx context.Context, user string) error {
 	//   InsertUser/foo would be too specific. There are other ways to append specific information
 	//   to a span.
 	//
-	_, span := otel.Tracer(name).Start(ctx, "InsertUser")
+	ctx2, span := otel.Tracer(name).Start(ctx, "InsertUser")
 	defer span.End()
 
 	time.Sleep(500 * time.Microsecond)
-	fmt.Println("User has been inserted!")
+
+	// We have gotten this context the last time we have created a span.
+	// Span corrolation is carried in the context.
+	LogOperation(ctx2, user)
 
 	return nil
+}
+
+func LogOperation(ctx context.Context, user string) {
+	// Just create another span.
+	_, span := otel.Tracer(name).Start(ctx, "LogOperation")
+	fmt.Printf("User '%s' has been added\n", user)
+	defer span.End()
 }
